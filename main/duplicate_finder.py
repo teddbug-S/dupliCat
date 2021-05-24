8d6d7d3073c7c1b9f017cfccdeac406d544f42ba
Lukasz Kwiecinski <lukkwie1811@gmail.com>
2021-05-24 17:03:42 +0200

    Removed unused imports

--- main/duplicate_finder.py
+++ main/duplicate_finder.py
@@ -1,8 +1,6 @@
-import argparse
 import typing
 import os
 from sys import exit
-from textwrap import dedent
 import logging
 
 from .resources.type_file import File
@@ -191,32 +189,3 @@ class DuplicateFinder:
             return result
         # If we've reached this point, return ``None``
         return None
-
-
-if __name__ == '__main__':
-
-    def main():
-        parser = argparse.ArgumentParser(description="Searches for duplicate files in a specified path.")
-        parser.add_argument("--path", '-p', default=os.curdir,
-                            help="specify path to look for duplicates, defaults to current directory")
-        parser.add_argument("--recurse", '-r', action='store_const', const=True,
-                            help="set this flag if you want search to be recursive which includes files in sub-dirs")
-        parser.add_argument("--by_hash", '-s', action='store_const', const=False, default=True,
-                            help="set this flag if you want to use size table otherwise, hash table.")
-        args = parser.parse_args()
-        finder = DuplicateFinder(path=args.path, recurse=args.recurse, by_hash=args.by_hash)
-        finder.search_duplicate()
-        analysis = finder.analysis
-        if analysis is not None:
-            temp = f"""
-            Total duplicates found: {analysis.total_count:,}
-            Total size on disk: {analysis.total_size:}
-            Most occurrence: {analysis.most_occurrence:,}
-            ---------------------------------
-            """
-            print(dedent(temp))
-        else:
-            print("[-] No duplicates found.")
-
-
-    main()
