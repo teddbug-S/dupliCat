01428816b439bc864d3469a853d546f01e9fe489
Divine Darkey <teddbug47@outlook.com>
2021-09-19 00:09:41 +0000

    Add files via upload

---
+++ resources/helpers.py
@@ -0,0 +1,54 @@
+from os import path
+from contextlib import redirect_stdout
+from collections import namedtuple
+from hashlib import blake2b
+from io import StringIO
+from functools import wraps
+import typing
+
+
+def silent(callback: typing.Callable):
+    @wraps(callback)
+    def wrapper(*args, **kwargs):
+        stream = StringIO()
+        with redirect_stdout(stream):
+            result = callback(*args, **kwargs)
+        return result
+    return wrapper
+
+
+def human_size(nbytes_: int) -> str:
+    """
+    Converts bytes to a human readable size
+
+        print(human_size(2024)) # -> 1.98 KB
+    """
+    suffixes = ["B", "KB", "MB", "GB", "TB", "PT"]
+    index = 0
+    while nbytes_ >= 1024 and index < len(suffixes) - 1:
+        nbytes_ /= 1024
+        index += 1
+    size = round(nbytes_, 2)
+    return f"{size:,} {suffixes[index]}"
+
+
+def read_chunk(file, size: int=400) -> bytes:
+    """ Reads first [size] chunks from file, size defaults to 400 """
+    file = path.join(file.root, file.name)  # get full path of file
+    with open(file, 'rb') as file:
+        # read chunk size
+        chunk = file.read(size)
+    return chunk
+
+
+def hash_chunk(file, key: int):
+    """ Returns a secure hash of first 1024 bytes from specified file. """
+    chunk = read_chunk(file, size=1024)  # read chunk
+    hash_ = blake2b(key=bytes(str(key), 'utf-8'))  # a hash object key set to file size
+    # update hash with chunk
+    hash_.update(chunk)
+    # return a secure hash
+    return hash_.hexdigest()
+
+
+Analysis = namedtuple("Analysis", ["total_count", "total_size", "most_occurrence"])
